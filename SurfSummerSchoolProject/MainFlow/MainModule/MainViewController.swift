ee0a0a2a2d33f028d96c195af64e61d4aeecee57
Ilya <ilya.klimenyuk@gmail.com>
2022-08-04 23:23:08 +0300

    Add collection view to main screen

--- SurfSummerSchoolProject/MainFlow/MainModule/MainViewController.swift
+++ SurfSummerSchoolProject/MainFlow/MainModule/MainViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK: - Constants
+
+    private enum Constants {
+        static let horisontalInset: CGFloat = 16
+        static let spaceBetweenElements: CGFloat = 7
+        static let spaceBetweenRows: CGFloat = 8
+    }
+
+    // MARK: - Private Properties
+
+    private let model: MainModel = .init()
+
+    // MARK: - Views
+
+    @IBOutlet private weak var collectionView: UICollectionView!
+
+    // MARK: - Lifeсyrcle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureApperance()
+        configureModel()
+        model.getPosts()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -18,7 +39,13 @@ class MainViewController: UIViewController {
         configureNavigationBar()
     }
     
-    private func configureNavigationBar() {
+}
+
+// MARK: - Private Methods
+
+private extension MainViewController {
+
+    func configureNavigationBar() {
         navigationItem.title = "Главная"
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "searchIcon"),
                                                             style: .plain,
@@ -26,8 +53,59 @@ class MainViewController: UIViewController {
                                                             action: #selector(searchButtonPressed(_:)))
     }
     
-    @objc private func searchButtonPressed(_ sender: UIBarButtonItem) {
+    @objc func searchButtonPressed(_ sender: UIBarButtonItem) {
         print("Search button pressed")
     }
     
+    func configureApperance() {
+        collectionView.register(UINib(nibName: "\(MainItemCollectionViewCell.self)", bundle: .main),
+                                forCellWithReuseIdentifier: "\(MainItemCollectionViewCell.self)")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.contentInset = .init(top: 10, left: 16, bottom: 10, right: 16)
+    }
+
+    func configureModel() {
+        model.didItemsUpdated = { [weak self] in
+            self?.collectionView.reloadData()
+        }
+    }
+
+}
+
+// MARK: - UICollection
+
+extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return model.items.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(MainItemCollectionViewCell.self)", for: indexPath)
+        if let cell = cell as? MainItemCollectionViewCell {
+            let item = model.items[indexPath.row]
+            cell.title = item.title
+            cell.isFavorite = item.isFavorite
+            cell.image = item.image
+            cell.didFavoritesTapped = { [weak self] in
+                self?.model.items[indexPath.row].isFavorite.toggle()
+            }
+        }
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemWidth = (view.frame.width - Constants.horisontalInset * 2 - Constants.spaceBetweenElements) / 2
+        return CGSize(width: itemWidth, height: 1.46 * itemWidth)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return Constants.spaceBetweenRows
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return Constants.spaceBetweenElements
+    }
+
 }
